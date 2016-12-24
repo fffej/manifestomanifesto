3387ff50cd46ffd4dee5f6250054adb80dc4f96d
Jeff Foster <jeff.foster@acm.org>
2016-12-24 09:20:29 +0000

    Remember to export the default class

--- src/BuildManifestoForm.js
+++ src/BuildManifestoForm.js
@@ -1,4 +1,6 @@
-class BuildManifestoForm extends React.Component {
+import React, { Component } from 'react';
+
+export default class BuildManifestoForm extends React.Component {
     constructor(props) {
 	super(props);
 	this.state = {value: ''};
